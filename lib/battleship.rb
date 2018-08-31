304ac3672268cf5830ad10161ac101a4bdc7ec1b
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-08-30 19:21:45 -0600

    create battleship game runner file

---
+++ lib/battleship.rb
@@ -0,0 +1,15 @@
+puts ""
+puts "Welcome to BATTLESHIP"
+puts ""
+puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+print ">"
+reply = gets.chomp.to_s.downcase
+
+if reply == "p"
+  p "Show ship layout"
+elsif reply == "i"
+  p "Show instructions"
+elsif reply == "q"
+end
+
+
