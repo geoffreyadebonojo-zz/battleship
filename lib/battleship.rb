8e3694027b0e2469303ee433e171f47c2545b175
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-02 15:01:37 -0600

    update battleship runner file to initiate two players and a game before start menu

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,3 +1,14 @@
+require './lib/player'
+require './lib/game'
+require './lib/space'
+require './lib/ship'
+require './lib/board'
+
+
+human = Player.new
+computer = Player.new
+game = Game.new(human, computer) 
+
 puts ""
 puts "Welcome to BATTLESHIP"
 puts ""
@@ -7,9 +18,12 @@ print ">"
 reply = gets.chomp.to_s.downcase
 if reply == "p"
   p "Show ship layout"
+  # game.ship_layout
 elsif reply == "i"
   p "Show instructions"
 elsif reply == "q"
 end
+
+
 
 
