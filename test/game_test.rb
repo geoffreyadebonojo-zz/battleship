af1da2ffbc14977847891b7b9e970cb32f989f91
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-02 12:37:01 -0600

    create Game runner class and first test

---
+++ test/game_test.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+  def setup
+    @game = Game.new
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+
+end
