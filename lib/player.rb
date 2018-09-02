71eaf45f829e6aa1f058189f0bc9e0c95b31c6ee
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-02 09:41:59 -0600

    remove current choice instance variable and change test

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,9 @@
 # player
 class Player
-  attr_reader :current_choice, :all_choices
+  attr_reader :all_choices
 
   def initialize
     @all_choices = []
-    @current_choice = @all_choices.last
   end
 
   def choose(chosen_space)
--- test/player_test.rb
+++ test/player_test.rb
@@ -19,16 +19,16 @@ class PlayerTest < Minitest::Test
   end
 
   def test_it_can_choose
-    space = Space.new(0,0)
+    space = Space.new("A2")
     @player.choose(space)
     assert_equal [space], @player.all_choices
   end
 
   def test_it_can_choose_several_times
-    space_A1 = Space.new(0,0)
-    space_A2 = Space.new(1,0)
-    space_A3 = Space.new(2,0)
-    space_A4 = Space.new(3,0)
+    space_A1 = Space.new("A1")
+    space_A2 = Space.new("A2")
+    space_A3 = Space.new("A3")
+    space_A4 = Space.new("A4")
     @player.choose(space_A1)
     @player.choose(space_A2)
     @player.choose(space_A3)
@@ -36,6 +36,19 @@ class PlayerTest < Minitest::Test
 
     expected = [space_A1, space_A2, space_A3, space_A4]
     assert_equal expected, @player.all_choices
+  end
+
+  def test_it_can_most_recent
+    space_A1 = Space.new("A1")
+    space_A2 = Space.new("A2")
+    space_A3 = Space.new("A3")
+    space_A4 = Space.new("A4")
+    @player.choose(space_A1)
+    @player.choose(space_A2)
+    @player.choose(space_A3)
+    @player.choose(space_A4)
+
+    assert_equal space_A4, @player.all_choices.last
   end
 
 end
