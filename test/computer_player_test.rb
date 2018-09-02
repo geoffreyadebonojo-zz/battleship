f72b80cb2aa185ee8c5dcdbe5d7cefe5206c7420
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-02 16:39:13 -0600

    add test for auto choice

--- test/computer_player_test.rb
+++ test/computer_player_test.rb
@@ -14,7 +14,14 @@ class ComputerPlayerTest < Minitest::Test
     assert_instance_of ComputerPlayer, @computer
   end
   
-  
+  def test_auto_choose_returns_array
+    @computer.auto_choose
+    assert_instance_of Array, @computer.all_choices
+  end
 
+  def test_auto_choose_pushes_space_to_array
+    @computer.auto_choose
+    assert_instance_of Space, @computer.all_choices.last
+  end
 
 end
