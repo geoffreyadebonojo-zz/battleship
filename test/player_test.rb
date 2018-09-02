c2887fd5decfdcefbebb61db2f20f12267b603e5
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-02 09:19:44 -0600

    create test for many choices made by player

--- test/player_test.rb
+++ test/player_test.rb
@@ -23,4 +23,19 @@ class PlayerTest < Minitest::Test
     @player.choose(space)
     assert_equal [space], @player.all_choices
   end
+
+  def test_it_can_choose_several_times
+    space_A1 = Space.new(0,0)
+    space_A2 = Space.new(1,0)
+    space_A3 = Space.new(2,0)
+    space_A4 = Space.new(3,0)
+    @player.choose(space_A1)
+    @player.choose(space_A2)
+    @player.choose(space_A3)
+    @player.choose(space_A4)
+
+    expected = [space_A1, space_A2, space_A3, space_A4]
+    assert_equal expected, @player.all_choices
+  end
+
 end
