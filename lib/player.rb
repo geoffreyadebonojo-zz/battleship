7aa0fe927ad7c388f1d5844860023a70536c00e0
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-05 00:04:07 -0600

    Complete rewrite

--- lib/player.rb
+++ lib/player.rb
@@ -1,78 +1,25 @@
 class Player
-  attr_reader   :all_choices,
-                :board
-  attr_accessor :ships
+  attr_reader :board, :shots
 
   def initialize(board)
     @board = board
-    @all_choices = []
-    @ships = []
+    @shots = []
   end
 
-  def choose(chosen_space)
-    @all_choices << chosen_space
+              #A2        #A3
+  def ship(space_one, space_two)
+
+    one = space_one.to_sym
+    two = space_two.to_sym
+
+    @board.hash[one].occupied = true
+    @board.hash[two].occupied = true
+
   end
 
-  def add_ship(ship)
-    @ships << ship
+              #A2
+  def shoot(space)
+    @shots << space.to_sym
   end
 
-  def build_fleet
-    puts "please choose two ships:"
-    puts "(s)ubmarine"
-    puts "(d)estoyer"
-    puts "(c)arrier"
-    puts "(b)attleship"
-    puts "............"
-    
-    2.times do |choice|
-      choice = gets.chomp.to_s.upcase
-      case choice
-      when "S" 
-        @ships << Ship.new("submarine")
-      when "D"
-        @ships << Ship.new("destroyer")
-      when "C"
-        @ships << Ship.new("carrier")
-      when "B"
-        @ships << Ship.new("battleship")
-      else
-        puts "please enter a valid ship type"
-      end
-    end
-    puts " "
-    puts "Your fleet:"
-    @ships.each do |ship|
-      puts ship.type
-    end
-  end
-
-  def select_space_on_board(coordinates)
-    chosen_space = @board.all_spaces_array.select do |space|
-      space.coordinates == coordinates
-    end 
-  end
-
-
-  def place_ship
-    p "enter the two character location e.g.(A3, B4, D1)"
-    p "to place your #{@ships.first.type}"
-    print ">"
-    @head = gets.chomp.to_s
-    select_space_on_board(reply)
-    # binding.pry
-    @ships[0].place_on("A2")
-    puts "Set tail of ship:"
-    valid_spaces = @ships[0].possible_tails(@ships[0].head_coordinates)
-    print valid_spaces
-    puts ""
-    print ">"
-    reply = gets.chomp.to_s
-    @ships[0].place_tail("A3")
-    
-    # binding.pry
-  end
 end
-
-#human = Player.new
-#computer = Player.new
--- test/player_test.rb
+++ test/player_test.rb
@@ -2,34 +2,13 @@ require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/player'
-require './lib/space'
-require './lib/ship'
 require './lib/board'
+require './lib/space'
+
 
 class PlayerTest < Minitest::Test
-
   def setup
-    all_spaces = [
-      @space_A1 = Space.new("A1"),
-      @space_A2 = Space.new("A2"),
-      @space_A3 = Space.new("A3"),
-      @space_A4 = Space.new("A4"),
-  
-      @space_B1 = Space.new("B1"),
-      @space_B2 = Space.new("B2"),
-      @space_B3 = Space.new("B3"),
-      @space_B4 = Space.new("B4"),
-  
-      @space_C1 = Space.new("C1"),
-      @space_C2 = Space.new("C2"),
-      @space_C3 = Space.new("C3"),
-      @space_C4 = Space.new("C4"),
-      
-      @space_D1 = Space.new("D1"),
-      @space_D2 = Space.new("D2"),
-      @space_D3 = Space.new("D3"),
-      @space_D4 = Space.new("D4")]
-    board = Board.new(all_spaces)
+    board = Board.new
     @player = Player.new(board)
   end
 
@@ -37,79 +16,29 @@ class PlayerTest < Minitest::Test
     assert_instance_of Player, @player
   end
 
-  def test_choices_starts_empty
-    assert_equal [], @player.all_choices
+  def test_it_has_board_as_attribute
+    assert_instance_of Board, @player.board
   end
 
-  def test_it_can_choose
-    space = Space.new("A2")
-    @player.choose(space)
-    assert_equal [space], @player.all_choices
+  def test_player_can_interact_with_board
+    @player.board.hash[:A0].occupied = true
+    @player.board.hash[:A0].status = "H"
+
+    assert_equal true, @player.board.hash[:A0].occupied
+    assert_equal "H", @player.board.hash[:A0].status
   end
 
-  def test_it_can_choose_several_times
-    space_A1 = Space.new("A1")
-    space_A2 = Space.new("A2")
-    space_A3 = Space.new("A3")
-    space_A4 = Space.new("A4")
-    @player.choose(space_A1)
-    @player.choose(space_A2)
-    @player.choose(space_A3)
-    @player.choose(space_A4)
-
-    expected = [space_A1, space_A2, space_A3, space_A4]
-    assert_equal expected, @player.all_choices
+  def test_ship_is_just_an_array_of_two_spaces
+    @player.ship("A2", "A3")
+    assert_equal true, @player.board.hash[:A2].occupied
+    assert_equal true, @player.board.hash[:A3].occupied
   end
 
-  def test_it_can_most_recent
-    space_A1 = Space.new("A1")
-    space_A2 = Space.new("A2")
-    space_A3 = Space.new("A3")
-    space_A4 = Space.new("A4")
-    @player.choose(space_A1)
-    @player.choose(space_A2)
-    @player.choose(space_A3)
-    @player.choose(space_A4)
-
-    assert_equal space_A4, @player.all_choices.last
-  end
-
-  def test_it_starts_without_ships
-    assert_equal [], @player.ships
-  end
-
-  def test_it_can_add_ships_to_fleet
-    submarine = Ship.new("submarine")
-    destroyer = Ship.new("destroyer")
-    @player.add_ship(submarine)
-    assert_equal [submarine], @player.ships
-    @player.add_ship(destroyer)
-    assert_equal [submarine, destroyer], @player.ships
-  end
-
-  def test_it_can_place_ships_on_board
-    skip
-    submarine = Ship.new("submarine")
-    destroyer = Ship.new("destroyer")
-    @player.add_ship(submarine)
-    @player.add_ship(destroyer)
-    
-    # binding.pry
-    # @player.place_ships
-    # => "enter the two character location e.g.(A3, B4, D1)"
-    # "to place your submarine"
-    # >A2
-    # Set tail of ship:
-    # ["B2", "A1", "A3"]
-    # >A1
-    # => A1
-  end
-
-  
-  def test_it_can_choose_space_from_board
-    expected = @player.board.all_spaces_array[2]
-    actual = @player.select_space_on_board("A3")
-    assert_equal [expected], actual
+  def test_it_shoots
+    @player.shoot("A1")
+    assert_equal [:A1], @player.shots
+    @player.shoot("A2")
+    assert_equal [:A1, :A2], @player.shots
   end
 
 end
