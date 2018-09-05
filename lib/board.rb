e17c2ed8773c9e2f7aa7bc5883f69ebc579816b4
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-05 00:00:41 -0600

    Complete rewrite

--- lib/board.rb
+++ lib/board.rb
@@ -1,68 +1,23 @@
-class Board 
-  attr_accessor :board, :all_spaces_array
+class Board
+  attr_reader :hash
 
-  def initialize(all_spaces_array)
-    @all_spaces_array = all_spaces_array
-    @board = {}
+  def initialize
+    @hash = generate
   end
 
-  def load_spaces
-    @board[:row_A] = @all_spaces_array[0..3]
-    @board[:row_B] = @all_spaces_array[4..7]
-    @board[:row_C] = @all_spaces_array[8..11]
-    @board[:row_D] = @all_spaces_array[12..15]
-    @board
-  end
-
-  def set_space_as_occupied(space)
-    row_symbol = "row_#{space.coordinates[0]}".to_sym
-    target_space = @board[row_symbol][space.coordinates[1].to_i - 1]
-    target_space.occupied = true
-  end
-
-  def check_for_hits(space)
-    @space = space
-    row_symbol = "row_#{@space.coordinates[0]}".to_sym
-    target_space = @board[row_symbol][@space.coordinates[1].to_i - 1]
-    if target_space.occupied
-      target_space.status = "H"
-    else 
-      target_space.status = "M"
+  def generate
+    hash = {}
+    up = [*'a'..'d'].map do |letter|
+      letter.upcase
     end
-    
+    up.each do |row|
+      4.times do |column|
+        formatted = (row.to_s + column.to_s).to_sym
+        hash[formatted] = Space.new(formatted)
+      end
+    end
+    # binding.pry
+
+    return hash
   end
-  
-
-  def display_board
-
-    a1 = @board[:row_A][0].status
-    a2 = @board[:row_A][1].status
-    a3 = @board[:row_A][2].status
-    a4 = @board[:row_A][3].status
-
-    b1 = @board[:row_B][0].status
-    b2 = @board[:row_B][1].status
-    b3 = @board[:row_B][2].status
-    b4 = @board[:row_B][3].status
-
-
-    c1 = @board[:row_C][0].status
-    c2 = @board[:row_C][1].status
-    c3 = @board[:row_C][2].status
-    c4 = @board[:row_C][3].status
-
-    d1 = @board[:row_D][0].status
-    d2 = @board[:row_D][1].status
-    d3 = @board[:row_D][2].status
-    d4 = @board[:row_D][3].status
-
-    puts "==========="
-    puts ". 1 2 3 4  "
-    puts "A #{a1} #{a2} #{a3} #{a4}" # use string
-    puts "B #{b1} #{b2} #{b3} #{b4}" # literals
-    puts "C #{c1} #{c2} #{c3} #{c4}" # to fill
-    puts "D #{d1} #{d2} #{d3} #{d4}" # out grid
-    puts "==========="
-  end
-
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,125 +1,28 @@
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
-
-require './lib/player'
-require './lib/computer_player'
-require './lib/game'
 require './lib/board'
 require './lib/space'
 
 class BoardTest < Minitest::Test
-	def setup
-		@all_spaces = [
-		@space_A1 = Space.new("A1"),
-		@space_A2 = Space.new("A2"),
-		@space_A3 = Space.new("A3"),
-		@space_A4 = Space.new("A4"),
+  def setup
+    @board = Board.new
+  end
 
-		@space_B1 = Space.new("B1"),
-		@space_B2 = Space.new("B2"),
-		@space_B3 = Space.new("B3"),
-		@space_B4 = Space.new("B4"),
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
 
-		@space_C1 = Space.new("C1"),
-		@space_C2 = Space.new("C2"),
-		@space_C3 = Space.new("C3"),
-		@space_C4 = Space.new("C4"),
-		
-		@space_D1 = Space.new("D1"),
-		@space_D2 = Space.new("D2"),
-		@space_D3 = Space.new("D3"),
-		@space_D4 = Space.new("D4")]
+  def test_it_can_generate
+    assert_instance_of Hash, @board.hash
+  end
 
+  def test_it_can_manipulate_spaces
+    @board.hash[:A3].occupied = true
+    @board.hash[:A3].status = "H"
 
-		human_board = Board.new(@all_spaces)
-		computer_board = Board.new(@all_spaces)
-
-		@human = Player.new(human_board)
-    computer = Player.new(computer_board)
-
-    @game = Game.new(@human, computer)
-
-	end
-	
-	def test_it_exists
-		assert_instance_of Game, @game
-	end
-
-	def test_it_loads_spaces
-		@human.board.load_spaces
-		refute_nil @human.board
-	end
-	
-	def test_it_can_set_spaces_as_occupied
-		@human.board.load_spaces
-		space = Space.new("C3")
-		row_symbol = "row_#{space.coordinates[0]}".to_sym
-		target_space = @human.board[row_symbol][space.coordinates[1].to_i - 1]
-
-		@human.board.set_space_as_occupied(space)
-		assert_equal true, target_space.occupied
-	end
-
-	def test_it_updates_hits
-		@human.board.load_spaces
-		space = Space.new("C3")
-		@human.board.set_space_as_occupied(space)
-		@human.board.check_for_hits(space)
-
-		assert_equal "H", @human.board[:row_C][2].status
-	end
-
-	def test_it_updates_misses
-		@human.board.load_spaces
-		
-		space = Space.new("C3")
-		@human.board.set_space_as_occupied(space)
-
-		target_space = Space.new("B1")
-		@human.board.check_for_hits(target_space)
-		# binding.pry
-
-		assert_equal "M", @human.board[:row_B][0].status
-	
-	end
-
-	def test_feature
-		skip
-		@human.board.load_spaces
-		space = Space.new("C3")
-		@human.board.set_space_as_occupied(space)
-		
-		first_target_space = Space.new("B1")
-		@human.board.check_for_hits(first_target_space)
-		
-		next_target_space = Space.new("C3")
-		@human.board.check_for_hits(next_target_space)
-
-		binding.pry
-		# @board.display_board =>
-		#
-		# ===========
-		# . 1 2 3 4
-		# A
-		# B M
-		# C     H
-		# D
-		# ===========
-
-		# @board.check_for_hits(Space.new("C4"))
-		# => "M"
-		#
-		# @board.display_board =>
-		#
-		# ===========
-		# . 1 2 3 4
-		# A
-		# B M
-		# C     H M
-		# D
-		# ===========
-
-	end
+    assert_equal true, @board.hash[:A3].occupied
+    assert_equal "H", @board.hash[:A3].status
+  end
 
 end
