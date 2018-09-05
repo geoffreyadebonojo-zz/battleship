7d068be0516f5b9a5fde6855c3652e7d8eb75e4c
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-04 23:58:51 -0600

    Complete rewrite

--- lib/game.rb
+++ lib/game.rb
@@ -1,32 +1,16 @@
+require 'pry'
+require './lib/game'
+require './lib/player'
 require './lib/board'
-
-class Game
-
-  attr_reader :player_1, :player_2
-
-  def initialize(human_player, computer_player)
-    @player_1 = human_player
-    @player_2 = computer_player
-  end
-
-  def player_1_guesses(space)
-    @player_1.choose(space)
-    @player_1.all_choices
-  end
-
-  def player_2_guesses(space)
-    @player_2.choose(space)
-    @player_2.all_choices
-  end
-
-  def player_1_ship_layout
-    @player_1.build_fleet
-    
-  end
-
-  def player_2_ship_layout
-    @player_2.auto_build_fleet
-  end
+require './lib/space'
 
 
-end
+
+
+board_1 = Board.new
+board_2 = Board.new
+player_1 = Player.new(board_1)
+player_2 = Player.new(board_2)
+game = Game.new(player_1, player_2)
+
+binding.pry
